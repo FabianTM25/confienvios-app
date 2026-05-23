ded8670d42b137a580ea674e765bc62ea027def7
YuderTM <yudertm25@gmail.com>
2026-05-23 15:03:53 -0500

    fix: jwtUtil

--- backend/src/main/java/com/YuderTM/security/JwtUtil.java
+++ backend/src/main/java/com/YuderTM/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.YuderTM.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
@@ -9,29 +10,35 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long EXPIRATION = 1000 * 60 * 60 * 8; // 8 horas
+  @Value("${JWT_SECRET}")
+  private String secretKey;
 
-    public String generarToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(key)
-                .compact();
-    }
+  private Key getKey() {
+    return Keys.hmacShaKeyFor(secretKey.getBytes());
+  }
 
-    public String extraerUsername(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build()
-                .parseClaimsJws(token).getBody().getSubject();
-    }
+  private final long EXPIRATION = 1000 * 60 * 60 * 8;
 
-    public boolean validarToken(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
-        } catch (JwtException e) {
-            return false;
-        }
+  public String generarToken(String username) {
+    return Jwts.builder()
+      .setSubject(username)
+      .setIssuedAt(new Date())
+      .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+      .signWith(getKey())
+      .compact();
+  }
+
+  public String extraerUsername(String token) {
+    return Jwts.parserBuilder().setSigningKey(getKey()).build()
+      .parseClaimsJws(token).getBody().getSubject();
+  }
+
+  public boolean validarToken(String token) {
+    try {
+      Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token);
+      return true;
+    } catch (JwtException e) {
+      return false;
     }
+  }
 }
