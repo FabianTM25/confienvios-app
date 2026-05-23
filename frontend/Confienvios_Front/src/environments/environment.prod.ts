59541fdde4af4bec9873dd8ca9dab213b3870280
YuderTM <yudertm25@gmail.com>
2026-05-22 22:41:30 -0500

    fix railway

--- frontend/Confienvios_Front/src/environments/environment.prod.ts
+++ frontend/Confienvios_Front/src/environments/environment.prod.ts
@@ -9,8 +9,7 @@ import packageInfo from '../../package.json';
 
 
 export const environment = {
+  appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://confienvios-app.onrender.com/api',
-  authUrl: 'https://confienvios-app.onrender.com/api/auth',
-  appVersion: '1.0.0'
+  apiUrl: 'https://confienvios-app-production.up.railway.app'
 };
--- frontend/Confienvios_Front/src/environments/environment.ts
+++ frontend/Confienvios_Front/src/environments/environment.ts
@@ -12,10 +12,9 @@ import packageInfo from '../../package.json';
 
 
 export const environment = {
+  appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://confienvios-app.onrender.com/api',
-  authUrl: 'https://confienvios-app.onrender.com/api/auth',
-  appVersion: '1.0.0'
+  apiUrl: 'https://confienvios-app-production.up.railway.app'
 };
 
 /*export const environment = {
