53aee40e981ac12a1d5b8d8c09a8092f20979569
YuderTM <yudertm25@gmail.com>
2026-05-23 10:13:43 -0500

    configuraciones

--- backend/src/main/java/com/YuderTM/security/SecurityConfig.java
+++ backend/src/main/java/com/YuderTM/security/SecurityConfig.java
@@ -126,10 +126,8 @@ import java.util.List;
 
       config.setAllowedOrigins(List.of(
         "http://localhost:4200",
-        "http://localhost:64764",
         "https://confienvios-app-two.vercel.app",
-        "https://confienvios-app.vercel.app",          // ←
-        "https://confienvios-app-fabiantm25s-projects.vercel.app"  // ← el tuyo
+        "https://confienvios-n2pn5wypp-fabiantm25s-projects.vercel.app"  // ← agrega este
       ));
 
       config.setAllowedMethods(List.of(
